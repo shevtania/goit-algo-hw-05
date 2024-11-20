0c9b45348894c95e8459b47f5251ac8e6fef8f31
Tetiana Shevtsova <shevtania@outlook.com>
2024-11-20 19:35:43 +0200

    after fixing

--- task2.py
+++ task2.py
@@ -2,13 +2,17 @@ import re
 from typing import Callable
 
 def generator_numbers(text: str):
-    for word in text.split():  # loop in list after spliting text
-        if re.search(r"\d+\.*\d+", word):  # find float number
-            yield float(word)
+    pattern = r"\s\d+\.?\d*\s" # pattern for number with spices on the sides
+    for word in re.finditer(pattern, text): # iterable finding word
+        num = float(word.group().strip()) # cut spaces and turn into float
+        yield num
     
+     
 def sum_profit(text: str, func: Callable):
 # sum numbers from generator    
     return sum(func(text))
 
-text = 'dfhwiefh  12 fwh 4.7 eofhi 14 lkdf 0.6 hwofh 64543 lefh 453.9787 n'
+#text = 'dfhwiefh  12 fwh 4.7 eofhi 14 lkdf 0.6 hwofh 64543 lefh 453.9787 n'
+text = "20.12 or 12.28 . 333.1  and 2.9  or 0.2 and 2 or4.4 end 5.5."
 print(sum_profit(text, generator_numbers))
+
